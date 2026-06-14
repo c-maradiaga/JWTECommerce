686bd8ab8583f0635a65b818f4c438514218e700
Carlos Maradiaga <carlos.maradiaga@hotmail.com>
2026-06-13 18:54:12 -0600

    Se actualizo el ProductProfile

--- Mapping/ProductProfile.cs
+++ Mapping/ProductProfile.cs
@@ -1,12 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace JWTECommerce.Mapping
+using AutoMapper;
+using JWTECommerce.Models;
+using JWTECommerce.Models.Dtos;
+
+namespace JWTECommerce.Mapping;
+
+public class ProductProfile: Profile
 {
-    public class ProductProfile
+    public ProductProfile()
     {
-        
-    }
+        CreateMap<Product, ProductDto>().ReverseMap();
+        CreateMap<Product, CreateProductDto>().ReverseMap();
+        CreateMap<Product, UpdateProductDto>().ReverseMap();
+
+    }    
 }
