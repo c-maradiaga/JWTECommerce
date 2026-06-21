05637bab27ab4c961535366ff8b09caf30ba3519
Carlos Maradiaga <carlos.maradiaga@hotmail.com>
2026-06-21 01:24:16 -0600

    Hasta modificar producto

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -137,6 +137,7 @@ public class ProductController : ControllerBase
         return CreatedAtRoute("GetProduct", new { productId = product.Id }, productoDto);
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     }
 
     /* Comprar articulo: El Patch es solo porque este verbo se asemeja mas a la accion que se va a hacer,
@@ -196,6 +197,8 @@ public class ProductController : ControllerBase
             return StatusCode(500, ModelState);
         }
         return NoContent();
+=======
+>>>>>>> f4bf33224c7c1302834cdabf121990b0e7bb0dbd
 =======
 >>>>>>> f4bf33224c7c1302834cdabf121990b0e7bb0dbd
 =======
